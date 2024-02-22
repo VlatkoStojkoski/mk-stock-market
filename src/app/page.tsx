93110c82b8c74d8d2a705f2d8a28e5e2768e9fa8
Vlatko Stojkoski <stojvlatko@gmail.com>
2024-02-22 13:29:36 +0100

    remove server calls to deploy api first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,13 @@ import { env } from "@/env";
 import { Table as ITable } from "@/lib/api";
 
 export default async function Home() {
-	console.log('fetching', env.NEXT_PUBLIC_URL);
-	const res = await fetch(new URL('/api', env.NEXT_PUBLIC_URL));
-	const data = await res.json() as ITable[];
+	// console.log('fetching', env.NEXT_PUBLIC_URL);
+	// const res = await fetch(new URL('/api', env.NEXT_PUBLIC_URL));
+	// const data = await res.json() as ITable[];
 
 	return (
 		<div className="container mx-auto min-h-screen p-8 flex flex-col items-start justify-center">
-			{
-				data ?
-					<StocksTable tables={data} /> :
-					<div className="text-3xl">Loading...</div>
-			}
+			{/* <StocksTable tables={data} /> */}
 		</div>
 	)
 }
