bc088a63628db8347f08c80beedff55a076e1c8c
Vlatko Stojkoski <stojvlatko@gmail.com>
2024-02-22 13:32:02 +0100

    add server calls back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { env } from "@/env";
 import { Table as ITable } from "@/lib/api";
 
 export default async function Home() {
-	// console.log('fetching', env.NEXT_PUBLIC_URL);
-	// const res = await fetch(new URL('/api', env.NEXT_PUBLIC_URL));
-	// const data = await res.json() as ITable[];
+	console.log('fetching', env.NEXT_PUBLIC_URL);
+	const res = await fetch(new URL('/api', env.NEXT_PUBLIC_URL));
+	const data = await res.json() as ITable[];
 
 	return (
 		<div className="container mx-auto min-h-screen p-8 flex flex-col items-start justify-center">
-			{/* <StocksTable tables={data} /> */}
+			<StocksTable tables={data} />
 		</div>
 	)
 }
